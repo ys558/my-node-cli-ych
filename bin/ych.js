a1894aa3d3173aa9c77bf6cfd56bc2431adcaa70
1 <yuyi.gz@163.com>
2021-10-19 18:32:20 +0800

    inquirer 交互式输入创建项目

--- bin/ych.js
+++ bin/ych.js
@@ -6,41 +6,53 @@ const inquirer = require('inquirer')
 // 获取package.json中的版本信息
 program.version(require("../package.json").version)
 
-program.option('-d, --description', 'ych 脚手架简介：。。。。')
+const questions = [
+	{
+		type: 'input',
+		name: 'projectName',
+		message: '请输入项目名称',
+	},
+	{
+		type: 'list',
+		name: 'frameWork',
+		message: '请选择框架',
+		default: 'React',
+		choices: ['React','Vue']
+	},
+]
 
-program.command('create <projectName>')
-	.option('-react')
-	.option('-vue')
-	.action((projectName, option) => {
-		const type = Object.keys(option)[0]
-		switch(type) {
-			case "Vue":
-				download(
-					'direct:git@github.com:ys558/ych-template.git',
-					projectName,
-					{ clone: true },
-					err => {
-						// 错误回调：
-						if (err) console.log(err)
-						console.log(`${projectName} 项目创建成功`)
-					}
-				)
-				break;
-			case "React":
-				download(
-					'direct:git@github.com:ys558/ych-template.git',
-					projectName,
-					{ clone: true },
-					(err) => {
-						// 错误回调：
-						if (err) console.log(err)
-						console.log(`${projectName} 项目创建成功`)
-					}
-				)
-				break;
-			default:
-				console.log("未添加参数，不能下载")
-		}
+program
+	.command('create')
+	.action(() => {
+		inquirer.prompt(questions)
+			.then(({projectName, frameWork}) => {
+				switch (frameWork) {
+					case 'React':
+						download(
+							'direct:git@github.com:ys558/ych-template.git#main',
+							projectName,
+							{ clone: true },
+							(err) => {
+								// 错误回调：
+								if (err) console.log(err)
+								console.log(`${projectName} 项目创建成功`)
+							})
+						break;
+					case 'Vue':
+						download(
+							'direct:git@github.com:ys558/ych-template.git#main',
+							projectName,
+							{ clone: true },
+							(err) => {
+								// 错误回调：
+								if (err) console.log(err)
+								console.log(`${projectName} 项目创建成功`)
+							})
+						break;
+					default:
+						break;
+				}
+			})
 	})
 
 
