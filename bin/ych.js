156a24d24095f82564e67ff37228ebf5416938d6
1 <yuyi.gz@163.com>
2021-10-21 13:48:22 +0800

    1

--- bin/ych.js
+++ bin/ych.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-const program = require('commander')
+const commander = require('commander')
 const download = require("download-git-repo")
 const inquirer = require('inquirer')
 const chalk = require('chalk')
+const figlet = require('figlet')
 
+// 将不同的commander进行分离，create只负责create命令的处理
+const program = new commander.Command()
+const create = program.command('create')
+	
 // 获取package.json中的版本信息
-program.version(require("../package.json").version)
+program.version(require('../package.json').version, '-v --version')
+
 
 const questions = [
 	{
@@ -22,32 +28,45 @@ const questions = [
 	},
 ]
 
-program
-	.command('create')
-	.action(() => {
+create
+	.usage('直接enter即可创建项目')
+	.description('创建项目命令')
+	// 在该命令加上 
+	.option('-ts', '项目用type script')
+	.option('-js', '项目用java script')
+	.action((option) => {
+		console.log(option)
 		inquirer.prompt(questions)
 			.then(({projectName, frameWork}) => {
 				switch (frameWork) {
 					case 'React':
 						download(
-							'direct:git@github.com:ys558/ych-template.git',
+							'direct:git@github.com:ys558/ych-template.git#main',
 							projectName,
 							{ clone: true },
 							(err) => {
 								// 错误回调：
-								if (err) console.log(chalk.bgYellow(err))
-								console.log(`${chalk.greenBright(projectName)} ${chalk.greenBright('项目创建成功')}`)
+								if (err) {
+									console.log(chalk.bgYellow(err))
+									return
+								}else{
+									console.log(`${chalk.bgGrey(projectName)} ${chalk.greenBright('项目创建成功')}`)
+								}
 							})
 						break;
 					case 'Vue':
 						download(
-							'direct:git@github.com:ys558/ych-template.git',
+							'direct:git@github.com:ys558/ych-template.git#main',
 							projectName,
 							{ clone: true },
 							(err) => {
 								// 错误回调：
-								if (err) console.log(chalk.bgYellow(err))
-								console.log(`${projectName} 项目创建成功`)
+								if (err) {
+									console.log(chalk.bgYellow(err))
+									return
+								}else{
+									console.log(`${chalk.bgGrey(projectName)} ${chalk.greenBright('项目创建成功')}`)
+								}
 							})
 						break;
 					default:
@@ -55,6 +74,5 @@ program
 				}
 			})
 	})
-
 
 program.parse(process.argv)
